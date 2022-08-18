ac60546faf8f53518ae3dc30bf21a3decb4b34e7
aribaP <ariba.mehdi2001@gmail.com>
2022-08-18 21:38:19 +0500

    Main.ts changes

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(
-    { 
+    {
       origin: '*',
       methods: ['POST', 'PUT', 'DELETE', 'GET', 'PATCH'],
       credentials: true
     });
-    await app.listen(5000);
+  const port = process.env.PORT || 5000;
+  app.enableCors();
+  await app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+  });
 }
 bootstrap();
